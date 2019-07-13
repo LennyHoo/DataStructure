d8c7c6f9eaf86a29646b593f994843e9f46f815b
LennyHoo <2570171677@qq.com>
2019-07-13 16:32:24 +0800

    2019-7-13 update

--- Queue（队列）/Queue（队列）/Queue.hpp
+++ Queue（队列）/Queue（队列）/Queue.hpp
@@ -69,7 +69,7 @@ public:
 		++this->size;
 		return OK;
 	}
-	T& DeQueue() {
+	T DeQueue() {
 		if (this->front == this->rear) throw "队列为空！";
 		QNode<T> *p = this->front->next;
 		T *copy = new T;
@@ -85,6 +85,67 @@ public:
 		while (p->next != nullptr) {
 			p = p->next;
 			visit(&(p->data));
+		}
+		return OK;
+	}
+};
+
+
+// 队列的顺序表示实现
+template <typename T>
+class Queue_Sq {
+private:
+	T *base;
+	int front;
+	int rear;
+	int MAXQSIZE;
+public:
+	Queue_Sq(int MAXQSIZE) {
+		this->MAXQSIZE = MAXQSIZE;
+		this->base = (T*)malloc(sizeof(T)*MAXQSIZE);
+		if (this->base == nullptr) exit(OVERFLOW);
+		this->front = this->rear = 0;
+	}
+	~Queue_Sq() {
+		if (this->base != nullptr) free(this->base);
+	}
+	Status InitQueue(int MAXQSIZE) {
+		this->MAXQSIZE = MAXQSIZE;
+		this->base = (T*)malloc(sizeof(T)*MAXQSIZE);
+		if (this->base == nullptr) exit(OVERFLOW);
+		this->front = this->rear = 0;
+		return OK;
+	}
+	Status ClearQueue() {
+		this->front = this->rear = 0;
+		return OK;
+	}
+	bool QueueEmpty() {
+		return (this->front == this->rear);
+	}
+	int QueueLength() {
+		return (this->rear - this->front + this->MAXQSIZE) % (this->MAXQSIZE);
+	}
+	T& GetHead() {
+		if (this->QueueEmpty()) throw "栈为空！";
+		return this->base[this->front];
+	}
+	Status EnQueue(T e) {
+		if ((this->rear + 1) % this->MAXQSIZE == this->front) throw "队列已满！";
+		this->base[this->rear] = e;
+		this->rear = (this->rear + 1) % this->MAXQSIZE;
+		return OK;
+	}
+	T& DeQueue() {
+		if (this->front == this->rear) throw "队列空！";
+		T *copy = new T;
+		*copy = this->base[this->front];
+		this->front = (this->front + 1) % this->MAXQSIZE;
+		return *copy;
+	}
+	Status QueueTraverse(pvist visit) {
+		for (int i = 0; i < this->QueueLength(); i++) {
+			visit(&(this->base[i]));
 		}
 		return OK;
 	}
